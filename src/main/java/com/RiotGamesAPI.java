b36f8d11489a30fdd5a6d28589686fcffef3fd86
Alec Asuncion <alec.r.asuncion@gmail.com>
2022-07-07 10:11:34 +0900

    Added encryption on property access and properties file

--- src/main/java/com/RiotGamesAPI.java
+++ src/main/java/com/RiotGamesAPI.java
@@ -1,12 +1,16 @@
 package com;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableEncryptableProperties
+@PropertySource(name = "EncryptedProperties", value = "classpath:encrypted.properties")
 public class RiotGamesAPI {
 
-	public static void main(String... args) {
-		SpringApplication.run(RiotGamesAPI.class, args);
-	}
+    public static void main(String... args) {
+        SpringApplication.run(RiotGamesAPI.class, args);
+    }
 }
